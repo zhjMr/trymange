3f13e07c8a12d04dceeb34af0cd030730ea3d412
ZhjMr <2744764939@qq.com>
2022-09-29 09:40:24 +0800

    依赖安装，跨域配置完成

--- mangage/vue.config.js
+++ mangage/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_BASE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          [`^${process.env.VUE_APP_BASE_API}`]: ""
+        }
+      }
+    }
+  }
 })
