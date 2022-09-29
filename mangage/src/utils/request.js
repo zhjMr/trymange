8626c9426a6df30639d23f2a5ce7c787dc2a66e2
ZhjMr <2744764939@qq.com>
2022-09-29 18:23:29 +0800

    响应拦截配置token

--- mangage/src/utils/request.js
+++ mangage/src/utils/request.js
@@ -1,20 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 //基准地址 超时时间
 const service = axios.create({
-    baseURL: 'http://119.45.133.128:8089/api',
-    timeout: 5000,
-})
+  baseURL: "http://119.45.133.128:8089/api",
+  timeout: 5000,
+});
 //请求拦截
-service.interceptors.request.use(config => {
-    return config
-}, error => {
-    return Promise.reject(error)
-})
+service.interceptors.request.use(
+  (config) => {
+    const token=localStorage.getItem("token")
+    if (token) config.headers.authorization = "Bearer" + token;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 //响应拦截
-service.interceptors.response.use(config => {
-    return config
-}, error => {
-    return Promise.reject(error)
-})
+service.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default service
+export default service;
--- mangage/src/views/HomeView.vue
+++ mangage/src/views/HomeView.vue
@@ -2,22 +2,23 @@
   <div class="card-box">
     <el-card class="card">
       <h3>系统登录</h3>
-      <el-form ref="form" :model="LoginForm" label-width="40px">
+      <el-form ref="form" :model="LoginForm" label-width="40px" :rules="rules">
         <el-form-item prop="username">
           <el-input
-            v-model="LoginForm.username"
+            v-model.trim="LoginForm.username"
             placeholder="请输入用户名"
           ></el-input>
         </el-form-item>
         <el-form-item prop="password">
           <el-input
-            v-model="LoginForm.password"
+            show-password
+            v-model.trim="LoginForm.password"
             placeholder="请输入密码"
           ></el-input>
         </el-form-item>
         <el-form-item prop="code" class="flax">
           <el-input
-            v-model="LoginForm.code"
+            v-model.trim="LoginForm.code"
             placeholder="请输入验证码"
             class="code"
           ></el-input>
@@ -41,9 +42,16 @@ export default {
       imgCodeUrl: "", //图片路径
       LoginForm: {
         //表单数据
-        username: "",
-        password: "",
+        username: "admin",
+        password: "1234",
         code: "",
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        code: [{ required: true, message: "请输入验证码", trigger: "blur" }],
       },
     };
   },
@@ -54,17 +62,24 @@ export default {
   methods: {
     //登录
     async PostLogin() {
-      try {
-        const response = await LoginApi.LoginUser(this.LoginForm);
-        if (response.data.code == 200) {
-          this.$message.success("登录成功");
-        } else if (response.data.code == 500) {
-          this.$message.error(response.data.msg);
+      const { username, password, code } = this.LoginForm;
+      if (username == "" || password == "" || code == "") {
+        this.$refs["form"].validate((valid) => {
+          if (!valid) return;
+        });
+      } else {
+        try {
+          const response = await LoginApi.LoginUser(this.LoginForm);
+          if (response.data.code == 200) {
+            this.$message.success("登录成功");
+            localStorage.setItem("token", response.data.token);
+          } else if (response.data.code == 500) {
+            this.$message.error(response.data.msg);
+          }
+          console.log(response);
+        } catch (e) {
+          console.log(e);
         }
-
-        console.log(response);
-      } catch (e) {
-        console.log(e);
       }
     },
     //验证码
